3986601ec81ba13d0251486180092b04f8dd67e2
SATO Yusuke <yusuke.sato.zz@gmail.com>
2019-08-08 14:27:47 +0900

    Add MergeCommand

--- src/resources/JGit.btm.ps1
+++ src/resources/JGit.btm.ps1
@@ -59,35 +59,35 @@ $JSON = @'
  {
    class: 'CheckoutCommand',
    start: '[checkout] Start checkout branch: ',
-   end: '[checkout] Checkout branch successfully: ',
+   end: '[checkout] Checkouted branch successfully: ',
    error: '[checkout] An error occurred while checkout: ',
    var: '$0.name'
  },
  {
    class: 'CloneCommand',
    start: '[clone] Start clone repository: ',
-   end: '[clone] Clone repository successfully: ',
+   end: '[clone] Cloned repository successfully: ',
    error: '[clone] An error occurred while clone repository: ',
    var: '$0.uri + " -> " + $0.branch'
  },
  {
    class: 'CreateBranchCommand',
    start: '[branch] Start create local branch: ',
-   end: '[branch] Create local branch successfully: ',
+   end: '[branch] Created local branch successfully: ',
    error: '[branch] An error occurred while create local branch: ',
    var: '$0.startPoint + " -> " + $0.name'
  },
  {
    class: 'CommitCommand',
    start: '[commit] Start commit to branch: ',
-   end: '[commit] Commit to branch successfully: ',
+   end: '[commit] Commited to branch successfully: ',
    error: '[commit] An error occurred while commit to branch: ',
    var: '$0.getRepository().getBranch()'
  },
  {
    class: 'FetchCommand',
    start: '[fetch] Start fetch from remote: ',
-   end: '[fetch] Fetch from remote successfully: ',
+   end: '[fetch] Fetched from remote successfully: ',
    error: '[fetch] An error occurred while fetch from remote: ',
    var: '$0.getRemote() + " -> " + $0.getRepository().getBranch()'
  }
@@ -124,7 +124,7 @@ BIND ret = $!.getStatus().toString()
 IF ret.equals("Ok")
 DO
   trace("log", java.time.ZonedDateTime.now().format(java.time.format.DateTimeFormatter.ISO_LOCAL_DATE_TIME) + "\t");
-  traceln("log", "[End][cherry-pick] Cherry-pick commit successfully: " + $0.ourCommitName + " -> " + $0.getRepository().getBranch());
+  traceln("log", "[End][cherry-pick] Cherry-picked commit successfully: " + $0.ourCommitName + " -> " + $0.getRepository().getBranch());
 ENDRULE
 
 RULE CherryPickCommand Conflicting
@@ -173,10 +173,110 @@ ENDRULE
 
 '@
 
+$MergeCommand = @'
+#############################################################################
+# MergeCommand
+#############################################################################
+RULE MergeCommand Open
+CLASS org.eclipse.jgit.api.MergeCommand
+METHOD call
+IF true
+DO traceOpen("log","byteman_" + java.net.InetAddress.getLocalHost().getHostName() + "_" + java.time.ZonedDateTime.now().format(java.time.format.DateTimeFormatter.ISO_LOCAL_DATE) + ".log")
+ENDRULE
+
+RULE MergeCommand Start
+CLASS org.eclipse.jgit.api.MergeCommand
+METHOD call
+AT ENTRY
+IF true
+DO
+  trace("log", java.time.ZonedDateTime.now().format(java.time.format.DateTimeFormatter.ISO_LOCAL_DATE_TIME) + "\t");
+  traceln("log", "[Start][merge] Start merge: " + $0.getRepository().getBranch());
+ENDRULE
+
+RULE MergeCommand End
+CLASS org.eclipse.jgit.api.MergeCommand
+METHOD call
+AT EXIT
+BIND ret = $!.getMergeStatus().toString()
+IF ret.equals("Fast-forward")
+OR
+ret.equals("Fast-forward-squashed")
+OR
+ret.equals("Already-up-to-date")
+OR
+ret.equals("Merged")
+OR
+ret.equals("Merged-squashed")
+OR
+ret.equals("Merged-squashed-not-committed")
+OR
+ret.equals("Merged-not-committed")
+DO
+  trace("log", java.time.ZonedDateTime.now().format(java.time.format.DateTimeFormatter.ISO_LOCAL_DATE_TIME) + "\t");
+  traceln("log", "[End][merge] Merged successfully: " + $0.getRepository().getBranch());
+ENDRULE
+
+RULE MergeCommand Conflicting
+CLASS org.eclipse.jgit.api.MergeCommand
+METHOD call
+AT EXIT
+BIND ret = $!.getMergeStatus().toString()
+IF ret.equals("Conflicting")
+OR
+ret.equals("Checkout Conflict")
+DO
+  trace("log", java.time.ZonedDateTime.now().format(java.time.format.DateTimeFormatter.ISO_LOCAL_DATE_TIME) + "\t");
+  traceln("log", "[Error][merge] Merge conflicted: " + $0.getRepository().getBranch());
+ENDRULE
+
+RULE MergeCommand Failed
+CLASS org.eclipse.jgit.api.MergeCommand
+METHOD call
+AT EXIT
+BIND ret = $!.getMergeStatus().toString()
+IF ret.equals("Failed")
+OR
+ret.equals("Aborted")
+OR
+ret.equals("Not-yet-supported")
+DO
+  trace("log", java.time.ZonedDateTime.now().format(java.time.format.DateTimeFormatter.ISO_LOCAL_DATE_TIME) + "\t");
+  traceln("log", "[Error][merge] Merge failed: " + $0.getRepository().getBranch());
+ENDRULE
+
+RULE MergeCommand Error
+CLASS org.eclipse.jgit.api.MergeCommand
+METHOD call
+AT THROW
+IF true
+DO
+  trace("log", java.time.ZonedDateTime.now().format(java.time.format.DateTimeFormatter.ISO_LOCAL_DATE_TIME) + "\t");
+  traceln("log", "[Error][merge] An error occurred while merge: " + $0.getRepository().getBranch());
+  traceln("log", $^.getMessage());
+  traceStack(null, "log");
+ENDRULE
+
+RULE MergeCommand Close
+CLASS org.eclipse.jgit.api.MergeCommand
+METHOD call
+AT EXIT
+IF true
+DO traceClose("log")
+ENDRULE
+
+
+
+'@
+
 ConvertFrom-Json $JSON | ForEach-Object { $_ } | ForEach-Object { ($Template -F $_.class, $_.start, $_.end, $_.error, $_.var) } `
     | % { [Text.Encoding]::UTF8.GetBytes($_) } `
     | Set-Content -Path ".\JGit.btm" -Encoding Byte
 
 $CherryPickCommand `
+    | % { [Text.Encoding]::UTF8.GetBytes($_) } `
+    | Add-Content -Path ".\JGit.btm" -Encoding Byte
+
+$MergeCommand `
     | % { [Text.Encoding]::UTF8.GetBytes($_) } `
     | Add-Content -Path ".\JGit.btm" -Encoding Byte
