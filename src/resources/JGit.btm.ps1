919488d06f70025ab6cee3b9037b5c1d04a1d1af
SATO Yusuke <yusuke.sato.zz@gmail.com>
2019-10-08 01:44:58 +0900

    Fix error before make repository

--- src/resources/JGit.btm.ps1
+++ src/resources/JGit.btm.ps1
@@ -1,6 +1,21 @@
+filter Invoke-Ternary ([scriptblock]$decider, [scriptblock]$ifTrue, [scriptblock]$ifFalse) 
+{
+    if (& $decider) { 
+        & $ifTrue
+    } else { 
+        & $ifFalse 
+    }
+}
+Set-Alias ?: Invoke-Ternary -Option AllScope -Description "PSCX filter alias"
+
 # Header wrote before log body
-$LogHeader = @'
-java.time.ZonedDateTime.now().format(java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS")) + "\t" + $0.getRepository().getDirectory().getParentFile().getName()
+# java.time.ZonedDateTime.now().format(java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS")) + "\t" + java.util.Optional.ofNullable($0.getRepository()).map(r -> r.getDirectory().getParentFile().getName()).orElse("-")
+$LogTime = @'
+java.time.ZonedDateTime.now().format(java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS")) + "\t" + 
+'@
+
+$LogRepoName = @'
+$0.getRepository().getDirectory().getParentFile().getName()
 '@
 
 # Template without detailed conditions
@@ -143,6 +158,7 @@ $JSON = @'
  {
    template: '$StandardTemplate',
    class: 'CheckoutCommand',
+   repoexist: 'true',
    start: '[checkout] Start checkout branch: ',
    end: '[checkout] Checkouted branch successfully: ',
    error: '[checkout] An error occurred while checkout: ',
@@ -151,6 +167,7 @@ $JSON = @'
  {
    template: '$StandardTemplate',
    class: 'CloneCommand',
+   repoexist: 'false',
    start: '[clone] Start clone repository: ',
    end: '[clone] Cloned repository successfully: ',
    error: '[clone] An error occurred while clone repository: ',
@@ -159,6 +176,7 @@ $JSON = @'
  {
    template: '$StandardTemplate',
    class: 'CreateBranchCommand',
+   repoexist: 'true',
    start: '[branch] Start create local branch: ',
    end: '[branch] Created local branch successfully: ',
    error: '[branch] An error occurred while create local branch: ',
@@ -167,6 +185,7 @@ $JSON = @'
  {
    template: '$StandardTemplate',
    class: 'CommitCommand',
+   repoexist: 'true',
    start: '[commit] Start commit to branch: ',
    end: '[commit] Commited to branch successfully: ',
    error: '[commit] An error occurred while commit to branch: ',
@@ -175,6 +194,7 @@ $JSON = @'
  {
    template: '$StandardTemplate',
    class: 'FetchCommand',
+   repoexist: 'true',
    start: '[fetch] Start fetch from remote: ',
    end: '[fetch] Fetched from remote successfully: ',
    error: '[fetch] An error occurred while fetch from remote: ',
@@ -183,6 +203,7 @@ $JSON = @'
  {
    template: '$StandardTemplate',
    class: 'PullCommand',
+   repoexist: 'true',
    start: '[pull] Start pull from remote: ',
    end: '[pull] Pulled from remote successfully: ',
    error: '[pull] An error occurred while pull: ',
@@ -191,6 +212,7 @@ $JSON = @'
  {
    template: '$StandardTemplate',
    class: 'PushCommand',
+   repoexist: 'true',
    start: '[push] Start push to remote: ',
    end: '[push] Pushed to remote successfully: ',
    error: '[push] An error occurred while push: ',
@@ -199,6 +221,7 @@ $JSON = @'
  {
    template: '$StandardTemplate',
    class: 'ResetCommand',
+   repoexist: 'true',
    start: '[reset] Start reset to remote: ',
    end: '[reset] Reset successfully: ',
    error: '[reset] An error occurred while reset: ',
@@ -207,6 +230,7 @@ $JSON = @'
  {
    template: '$StandardTemplate',
    class: 'RevertCommand',
+   repoexist: 'true',
    start: '[revert] Start revert: ',
    end: '[revert] Revert successfully: ',
    error: '[revert] An error occurred while revert: ',
@@ -215,6 +239,7 @@ $JSON = @'
  {
    template: '$StandardTemplate',
    class: 'StashApplyCommand',
+   repoexist: 'true',
    start: '[stash apply] Start apply stash to branch: ',
    end: '[stash apply] Apply stash to branch successfully: ',
    error: '[stash apply] An error occurred while apply stash to branch: ',
@@ -222,6 +247,7 @@ $JSON = @'
  },
  {
    template: '$StandardTemplate',
+   repoexist: 'true',
    class: 'StashCreateCommand',
    start: '[stash create] Start creating stash: ',
    end: '[stash create] Created stash successfully: ',
@@ -238,6 +264,7 @@ $ExtendedJSON = @'
  {
    template: '$ExtendedTemplate',
    class:    'CherryPickCommand',
+   repoexist: 'true',
    start:    '[cherry-pick] Start cherry-pick: ',
    end:      '[cherry-pick] Cherry-picked commit successfully: ',
    error:    '[cherry-pick] An error occurred while cherry-pick: ',
@@ -260,6 +287,7 @@ $ExtendedJSON = @'
  {
    template: '$ExtendedTemplate',
    class:    'MergeCommand',
+   repoexist: 'true',
    start:    '[merge] Start merge: ',
    end:      '[merge] Merged successfully: ',
    error:    '[merge] An error occurred while merge: ',
@@ -291,6 +319,7 @@ $ExtendedJSON = @'
  {
    template: '$ExtendedTemplate',
    class:    'RebaseCommand',
+   repoexist: 'true',
    start:    '[rebase] Start rebase: ',
    end:      '[rebase] Rebased branch successfully: ',
    error:    '[rebase] An error occurred while rebase: ',
@@ -325,7 +354,7 @@ $ExtendedJSON = @'
 ConvertFrom-Json $JSON | ForEach-Object { $_ } `
     | ForEach-Object { 
         $template = $_.template;
-        (Invoke-Expression "Write-Output $template") -F $LogHeader, $_.class, $_.start, $_.end, $_.error, $_.var
+        (Invoke-Expression "Write-Output $template") -F ($_.repoexist | ?: {$_ -eq 'true'} {$LogTime + $LogRepoName} {$LogTime + '"-"'}), $_.class, $_.start, $_.end, $_.error, $_.var
       } `
     | % { [Text.Encoding]::UTF8.GetBytes($_) } `
     | Set-Content -Path ".\JGit.btm" -Encoding Byte
@@ -333,8 +362,64 @@ ConvertFrom-Json $JSON | ForEach-Object { $_ } `
 ConvertFrom-Json $ExtendedJSON | ForEach-Object { $_ } `
     | ForEach-Object { 
         $template = $_.template;
-        (Invoke-Expression "Write-Output $template") -F $LogHeader, $_.class, $_.start, $_.end, $_.error, $_.var, ($_.normalCondition -join "`n"), ($_.conflictCondition -join "`n"), $_.conflict, ($_.failedCondition -join "`n"), $_.failed
+        (Invoke-Expression "Write-Output $template") -F ($_.repoexist | ?: {$_ -eq 'true'} {$LogTime + $LogRepoName} {$LogTime + '"-"'}), $_.class, $_.start, $_.end, $_.error, $_.var, ($_.normalCondition -join "`n"), ($_.conflictCondition -join "`n"), $_.conflict, ($_.failedCondition -join "`n"), $_.failed
       } `
     | % { [Text.Encoding]::UTF8.GetBytes($_) } `
     | Add-Content -Path ".\JGit.btm" -Encoding Byte
+
+
+
+$PushTransport = @'
+#############################################################################
+# PushTransport
+#############################################################################
+RULE PushTransport Open
+CLASS org.eclipse.jgit.transport.{1}
+METHOD open(org.eclipse.jgit.lib.Repository, org.eclipse.jgit.transport.URIish, String)
+IF true
+DO
+  traceOpen("log","byteman_" + java.net.InetAddress.getLocalHost().getHostName() + "_" + java.time.ZonedDateTime.now().format(java.time.format.DateTimeFormatter.ISO_LOCAL_DATE) + ".log");
+  trace("log", {0} + $1.getDirectory().getParentFile().getName() + "\t");
+  traceln("log", "[Start]{2}" + $1.getBranch() + " -> " + $2.toString());
+ENDRULE
+
+RULE PushTransport End
+CLASS org.eclipse.jgit.transport.{1}
+METHOD push
+AT EXIT
+IF true
+DO
+  trace("log", {0} + "-" + "\t");
+  traceln("log", "[End]{3}" + {5});
+ENDRULE
+
+RULE PushTransport Error
+CLASS org.eclipse.jgit.transport.{1}
+METHOD push
+AT THROW
+IF true
+DO
+  trace("log", {0} + "-" + "\t");
+  traceln("log", "[Error]{4}" + {5});
+  traceln("log", $^.getMessage());
+  traceStack(null, "log");
+ENDRULE
+
+RULE PushTransport Close
+CLASS org.eclipse.jgit.transport.{1}
+METHOD push
+AT EXIT
+IF true
+DO traceClose("log")
+ENDRULE
+
+
+
+'@
+
+$PushTransport -F $LogTime, 'Transport', '[push] Start push to remote: ', '[push] Pushed to remote successfully: ', '[push] An error occurred while push: ', '""' `
+    | % { [Text.Encoding]::UTF8.GetBytes($_) } `
+    | Add-Content -Path ".\JGit.btm" -Encoding Byte
+
+
 
